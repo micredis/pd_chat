6a46d84d309afb8c54addfeef39116e161b2240e
Dmitriy Platonov <dplatonov@eshopworld.com>
2019-11-19 13:25:39 +0200

    Make component About

--- src/main/resources/client/src/app/app.module.ts
+++ src/main/resources/client/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import {AboutComponent} from "./component/about/about.component";
 import { NavigationComponent } from './component/navigation/navigation.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {LayoutModule} from "@angular/cdk/layout";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
 import {MatDialogModule} from "@angular/material/dialog";
+import {FlexLayoutModule, FlexModule} from "@angular/flex-layout";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -31,7 +33,10 @@ import {MatDialogModule} from "@angular/material/dialog";
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatDialogModule
+    MatDialogModule,
+    FlexLayoutModule,
+    FlexModule,
+    HttpClientModule
   ],
   entryComponents:[
     AboutComponent
--- src/main/resources/client/src/app/component/about/about.component.ts
+++ src/main/resources/client/src/app/component/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  url = '/actuator/info';
+  version: string;
+  buildDate: string;
+
+  constructor(private http: HttpClient, public dialogRef: MatDialogRef<AboutComponent>) {
+  }
 
   ngOnInit() {
+    this.getInfo().then(response => {
+      this.version = response.version;
+      this.buildDate = response.buildDate;
+    })
+  }
+
+  getInfo() {
+    return this.http.get(this.url)
+    .toPromise()
+    .then(response => response.toString())
+    .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
   }
 
 }
